1e7fb78ec0f6bd6d361d7e9daef1da7ba08d7937
KimrAndrew <andrewrk1997@gmail.com>
2022-06-09 17:34:01 -0700

    fixes relation to Type on Pokemon model

--- Games/models/pokemon.py
+++ Games/models/pokemon.py
@@ -1,32 +1,12 @@
 from django.db import models
-
+from Games.models.types import Type
 class Pokemon(models.Model):
     name = models.CharField(max_length=32)
     attack = models.IntegerField()
     special_attack = models.IntegerField()
     defense = models.IntegerField()
     special_defense = models.IntegerField()
-    types = models.ManyToManyField(to=)
+    types = models.ManyToManyField(to=Type)
 
-class Type(models.Model):
-    TYPE_CHOICES = [
-        ('FIR','Fire'),
-        ('GRS','Grass'),
-        ('WTR','Water'),
-        ('BUG','Bug'),
-        ('NRM','Normal'),
-        ('PSN','Poison'),
-        ('ELC','Electric'),
-        ('GRD','Ground'),
-        ('FGT','Fighting'),
-        ('PSY','Psychic'),
-        ('RCK','Rock'),
-        ('GHT','Ghost'),
-        ('ICE','Ice'),
-        ('DRG','Dragon'),
-        ('DRK','Dark'),
-        ('STL','Steel'),
-        ('FLY','Flying')
-    ]
-    type = models.CharField(max_length=3,choices=TYPE_CHOICES)
+
 
