f13464f42edc0432f3691acd9736d9e39d7aad8e
KimrAndrew <andrewrk1997@gmail.com>
2022-06-09 20:27:47 -0700

    finishes making each type track how it takes damage

--- Games/models/types.py
+++ Games/models/types.py
@@ -18,6 +18,13 @@ class Type(models.Model):
         ('DRG','Dragon'),
         ('DRK','Dark'),
         ('STL','Steel'),
-        ('FLY','Flying')
+        ('FLY','Flying'),
+        ('FAI','Fairy')
     ]
     type = models.CharField(max_length=3,choices=TYPE_CHOICES)
+    resistances = models.ManyToManyField(to="self",blank=True,symmetrical=False,related_name='resistance_set')
+    immunities = models.ManyToManyField(to="self",blank=True,symmetrical=False,related_name='immunities_set')
+    weaknesses = models.ManyToManyField(to="self",blank=True,symmetrical=False,related_name='weaknesses_set')
+
+    def __str__(self):
+        return self.type
